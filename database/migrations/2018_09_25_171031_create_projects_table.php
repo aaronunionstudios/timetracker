75abfd781ea2e8f48189899e6923d32228966dc6
Aaron Wilson <adwilso@castlebranch.com>
2018-09-25 13:46:51 -0400

    saving changes to models and controlers

--- database/migrations/2018_09_25_171031_create_projects_table.php
+++ database/migrations/2018_09_25_171031_create_projects_table.php
@@ -6,24 +6,17 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProjectsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('projects');
--- database/migrations/2018_09_25_171053_create_timers_table.php
+++ database/migrations/2018_09_25_171053_create_timers_table.php
@@ -1,31 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
 
-class CreateTimersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateTimersTable extends Migration
     {
-        Schema::create('timers', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+        public function up()
+        {
+            Schema::create('timers', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->unsignedInteger('project_id');
+                $table->unsignedInteger('user_id');
+                $table->timestamp('started_at');
+                $table->timestamp('stopped_at')->default(null)->nullable();
+                $table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('timers');
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('project_id')->references('id')->on('projects');
+            });
+        }
+
+        public function down()
+        {
+            Schema::dropIfExists('timers');
+        }
     }
-}
